70aabbb6f7938b29dd0288eee1f13e46b9821442
Ernest Mazur <emazemhs@gmail.com>
2021-10-08 01:38:16 +0200

    AUTOCOMMIT: 08-10-2021_01:38:16

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\OptAd;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,9 +17,12 @@ class MainController extends AbstractController
         /*$url = "https://api.optad360.com/get?key=HJGHcZvJHZhjgew6qe67q6GHcZv3fdsAqxbvB33fdV&startDate=2021-08-11&endDate=2021-08-11&output=json";
 
         dump(file_get_contents($url));*/
+        $em = $this->getDoctrine()->getManager();
+        $opts = $em->getRepository(OptAd::class)->findAll();
 
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
+            'opts' => $opts
         ]);
     }
 }
